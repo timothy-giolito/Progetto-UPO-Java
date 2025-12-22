9bc18a3ba6f8cd423f82654a997bfd6efb3cb2b5
0406-0406 <20054744@studenti.uniupo.it>
2025-12-22 10:28:46 +0100

    ListaDiArticoliException.java

--- src/modello/eccezioni/ListaDiArticoliException.java
+++ src/modello/eccezioni/ListaDiArticoliException.java
@@ -2,4 +2,24 @@ package modello.eccezioni;
 
 public class ListaDiArticoliException extends Exception{
 
-}
+    private static final long serialVersionUID = 1L;
+
+    //costruttore senza parametro
+    public ListaDiArticoliException()
+    {
+        super();
+    }
+
+    //costruttore con messaggio di errore
+    public ListaDiArticoliException(String messaggio)
+    {
+        super(messaggio);
+    }
+
+    //costruttore con messaggio e causa 
+    public ListaDiArticoliException(String messaggio, Throwable causa)
+    {
+        super(messaggio, causa);
+    }
+
+} 
