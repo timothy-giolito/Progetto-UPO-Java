4e02887d750d37fb5841dfee2aa01e16dc05dae4
TG20054431 <20054431@studenti.uniupo.it>
2025-12-22 10:31:08 +0100

    GestioneListeException finito

--- src/modello/GestioneListe.java
+++ src/modello/GestioneListe.java
@@ -1,5 +1,17 @@
 package modello;
 
+import java.util.ArrayList;
+import java.util.List;
+import modello.eccezioni.GestioneListeException;
+
 public class GestioneListe {
 
+	// definizione lista
+	private List<ListaDiArticoli> Liste;
+	
+	// costruttore di ArrayList per contenere una nuova lista di articoli
+	public GestioneListe() {
+		
+		this.Liste = new ArrayList<>();
+	}
 }
--- src/modello/eccezioni/GestioneListeException.java
+++ src/modello/eccezioni/GestioneListeException.java
@@ -1,5 +1,12 @@
 package modello.eccezioni;
 
-public class GestioneListeException {
+public class GestioneListeException extends Exception{
+	private static final long serialVersionUID = 1L;
 
+	// costruttore con parametro
+	public GestioneListeException(String messaggio) {
+		
+		super(messaggio);
+		
+	}
 }
