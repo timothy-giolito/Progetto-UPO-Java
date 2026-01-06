08ff0561848b4996e21bd31b6da7bc7ed587e77d
TG20054431 <20054431@studenti.uniupo.it>
2026-01-06 18:55:50 +0100

    javadoc main

--- src/interfaccia/cli/RigaDiComando.java
+++ src/interfaccia/cli/RigaDiComando.java
@@ -1,7 +1,20 @@
 package interfaccia.cli;
+/**
+ * Gestisce l'interazione con l'utente tramite terminale (Command Line Interface).
+ * <p>
+ * Questa classe si occupa di mostrare i menu testuali, leggere gli input dell'utente
+ * e invocare i metodi del modello ({@link modello.GestioneListe}) per eseguire le operazioni richieste.
+ * </p>
+ * @author Timothy Giolito 20054431
+ * @author Luca Franzon 20054744
+ */
 
 public class RigaDiComando {
 
+	/**
+     * Avvia il ciclo principale dell'interfaccia testuale.
+     * Mantiene l'applicazione attiva finché l'utente non sceglie di uscire.
+     */
 	public void start() {
 		// TODO Auto-generated method stub
 		
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,31 @@ import java.util.Scanner;
 import interfaccia.cli.RigaDiComando;
 import interfaccia.grafica.InterfacciaGrafica;
 
+/**
+ * Classe principale (Entry Point) dell'applicazione "Progetto UPO Java".
+ * <p>
+ * Questa classe gestisce l'avvio del programma permettendo all'utente di scegliere
+ * la modalità di interazione desiderata:
+ * <ul>
+ * <li><b>Interfaccia a Riga di Comando (CLI):</b> per un utilizzo testuale nel terminale.</li>
+ * <li><b>Interfaccia Grafica (GUI):</b> per un utilizzo tramite finestre e pulsanti.</li>
+ * </ul>
+ * </p>
+ * @author Timothy Giolito 20054431
+ * @author Luca Franzon 20054744
+ * @version 1.0
+ */
+
 public class Main {
 
+	/**
+     * Metodo principale che avvia l'esecuzione del programma.
+     * <p>
+     * Richiede all'utente di selezionare la modalità di avvio tramite input da tastiera
+     * e istanzia la classe di interfaccia corrispondente ({@link RigaDiComando} o {@link InterfacciaGrafica}).
+     * </p>
+     * @param args Argomenti da riga di comando (non utilizzati in questa versione).
+     */
     public static void main(String[] args) {
 
         // Scelta dell'interfaccia utente
