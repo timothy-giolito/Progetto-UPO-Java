bfa89634afb480b6405d2a5e9bc25254676d30f9
TG20054431 <20054431@studenti.uniupo.it>
2026-01-06 18:56:41 +0100

    javadoc ui

--- src/interfaccia/grafica/InterfacciaGrafica.java
+++ src/interfaccia/grafica/InterfacciaGrafica.java
@@ -1,5 +1,15 @@
 package interfaccia.grafica;
 
+/**
+ * Gestisce l'interazione con l'utente tramite interfaccia grafica (GUI).
+ * <p>
+ * Costruisce e visualizza le finestre, i pannelli e i componenti grafici (Swing)
+ * necessari per la gestione visuale delle liste della spesa e degli articoli.
+ * </p>
+ * @author Timothy Giolito 20054431
+ * @author Luca Franzon 20054744
+ */
+
 public class InterfacciaGrafica {
 
 }
