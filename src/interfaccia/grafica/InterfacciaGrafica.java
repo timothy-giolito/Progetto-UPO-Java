52325886de14146ab7c527f05275783086455a18
TG20054431 <20054431@studenti.uniupo.it>
2026-01-07 16:39:49 +0100

    Inizio GUI

--- src/interfaccia/grafica/InterfacciaGrafica.java
+++ src/interfaccia/grafica/InterfacciaGrafica.java
@@ -10,8 +10,13 @@ import modello.*;
 import modello.eccezioni.*;
 
 /**
- * Interfaccia Grafica Swing Completa.
- * Include gestione Liste, Cestino, Ripristino e Catalogo Globale.
+ * Gestisce l'interazione con l'utente tramite interfaccia grafica (GUI).
+ * <p>
+ * Costruisce e visualizza le finestre, i pannelli e i componenti grafici (Swing)
+ * necessari per la gestione visuale delle liste della spesa e degli articoli.
+ * </p>
+ * @author Timothy Giolito 20054431
+ * @author Luca Franzon 20054744
  */
 public class InterfacciaGrafica extends JFrame {
 
